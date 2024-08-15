8465c266863a03abea5fcac82833fbee5bd424f8
ярослав <yaracrut245@gmail.com>
2024-08-15 15:28:05 +0500

    1

--- src/Task2/MainTask2.kt
+++ src/Task2/MainTask2.kt
@@ -1,2 +1,7 @@
 package Task2
 
+fun main(){
+    var num = 25.42
+    var num1  =num.toInt()
+    println(num1)
+}
--- src/Task4/MainTask4.kt
+++ src/Task4/MainTask4.kt
@@ -1,2 +1,11 @@
 package Task4
 
+fun main (){
+    var a = 8
+    var b = 5
+    if (a > b){
+        a -= 3
+        b += 3
+        println("a = $a | b = $b")
+    }
+}
