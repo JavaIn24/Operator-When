8c23c0b130aa56a57a97bcb3ea9e88fbf554ed2e
JavaIn24 <yaracrut245@gmail.com>
2024-08-27 15:36:32 +0500

    Update MainTask2.kt

--- src/Task2/MainTask2.kt
+++ src/Task2/MainTask2.kt
@@ -1,7 +1,8 @@
 package Task2
 
 fun main(){
-    var num = 25.42
-    var num1  =num.toInt()
-    println(num1)
+    var num = 25.64
+    val df = DecimalFormat("#")
+    df.roundingMode = RoundingMode.CEILING
+    println(df.format(num))
 }
